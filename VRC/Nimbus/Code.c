fbb03e2648ea25d44c37a83de87242f44f22c26a
User15937 <lotsofmath@gmail.com>
2013-11-05 16:42:14 -0600

    Corrected template flaw

--- VRC/Nimbus/Code.c
+++ VRC/Nimbus/Code.c
@@ -16,7 +16,7 @@
 //Competition Control and Duration Settings
 #pragma competitionControl(Competition)
 #pragma autonomousDuration(15)
-#pragma userControlDuration(120)
+#pragma userControlDuration(105)
 
 #include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
 
