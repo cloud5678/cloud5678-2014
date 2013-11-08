addecae54d48443a971307411b5026a35d3aac72
erik <erik.ostlind@att.net>
2013-11-08 08:35:23 -0600

    i posted the wrong code yesterday and this is the right code

--- VRC/Arm/CheeseArm/cheeseArm.c
+++ VRC/Arm/CheeseArm/cheeseArm.c
@@ -1,7 +1,8 @@
-#pragma platform(VEX)
-
-#pragma competitionControl(Competition)
-#pragma autonomousDuration(2000)
+#pragma config(Sensor, in1,   Base,          	sensorNone)
+#pragma config(Sensor, in2,   Shoulder,       sensorNone)
+#pragma config(Sensor, in3,   Elbow,          sensorNone)
+#pragma config(Sensor, in4,   Wrist,          sensorNone)
+#pragma config(Sensor, in5,		Bumper,					sensorTouch)
 #pragma config(Motor, port1,	baseMotor1, 			tmotorVex393,	openLoop)
 #pragma config(Motor, port10,	baseMotor2, 			tmotorVex393,	openLoop, reversed)
 #pragma config(Motor, port2,	shoulderMotor1, 	tmotorVex393,	openLoop)
@@ -13,16 +14,78 @@
 #pragma config(Motor, port8,	wristMotorRot,		tmotorVex269,	openLoop)
 #pragma config(Motor, port9,	clawMotor,				tmotorVex269,	openLoop)
 
+#pragma platform(VEX)
+
+#pragma competitionControl(Competition)
+
+//#include "PIDController.c"
 #include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+
+
+
+typedef struct {
+	bool enabled;
+	float kP, kI, kD;
+	int setpoint;
+	int maxOutput, minOutput;
+	int error, totalError, prevError;
+} PIDController;
+
+void init(PIDController controller, float kP, float kI, float kD) {
+	controller.kP = kP;
+	controller.kI = kI;
+	controller.kD = kD;
+}
+void setSetpoint(PIDController controller, int setpoint) {
+	controller.setpoint = setpoint;
+	controller.totalError = 0.0;
+	controller.prevError = 0.0;
+}
+void setThresholds(PIDController controller, int max, int min) {
+	controller.maxOutput = max;
+	controller.minOutput = min;
+}
+void setEnabled(PIDController controller, bool en) {
+	controller.enabled = en;
+}
+int calculate(PIDController controller, int input) {
+	if (!controller.enabled) {
+		return 0.0;
+	}
+	controller.error = controller.setpoint - input;
+	controller.totalError += controller.error;
+
+	int output = controller.kP * controller.error +
+               controller.kI * controller.totalError +
+               controller.kD * (controller.prevError - controller.error);
+
+	if (controller.maxOutput != 0 && controller.minOutput != 0) {
+		if (output > controller.maxOutput) {
+			output = controller.maxOutput;
+		}
+		if (output < controller.minOutput) {
+			output = controller.minOutput;
+		}
+	}
+
+	controller.prevError = controller.error;
+	return output;
+}
+
+
+
 
 const int upperArm = 29; //cm
 const int lowerArm = 0;	 //cm
-const int bowlDist = 10; //cm
+const int bowlDist = 20; //cm
 const int bowlDiam = 10; //cm
+int shoulderZero;
+int elbowZero;
+int wristZero;
 
 PIDController shoulderPID;
 PIDController elbowPID;
-PIDController wristPID;
+PIDController basePID;
 
 void pre_auton()
 {
@@ -40,36 +103,49 @@ void setElbowValue(int val)
 {
 		motor[elbowMotor1] = motor[elbowMotor2] = val;
 }
-void setWristValue(int val)
+void setWristVertValue(int val)
 {
 		motor[wristMotorVert1] = motor[wristMotorVert2] = val;
 }
-
-void keepHandLevel(int insideAngle)
+void setWristRotValue(int val)
 {
-	 //180-insideAngle;
+		motor[wristMotorRot] =  val;
 }
+void setBaseValue(int val)
+{
+		motor[baseMotor1] = motor[baseMotor2] = val;
+}
+void setClawValue(int val)
+{
+	 motor[clawMotor] = val;
+}
+
+
 
 void bendArm(int x, int y)
 {
 		int A = lowerArm;
 		int B = upperArm;
-		double a1,a2,a3;
-		double C = sqrt((x*x)+(y*y));
+		float a1,a2;
+		float C = sqrt((x*x)+(y*y));
 		a1 = acos(((C*C)+(B*B)-2*C*B)/(A*A));
 		a2 = acos(((A*A)+(B*B)-2*A*B)/(C*C));
 		setSetpoint(shoulderPID,a1);
-		setEnabled(shoulderPID,true);
 		setSetpoint(elbowPID,a2);
+		//setSetpoint(wristPID,180-(180-(a1+a2)));
+		setEnabled(shoulderPID,true);
 		setEnabled(elbowPID,true);
-		setSetpoint(wristPID,180-(180-(a1+a2)));
-		setEnabled(wristPID,true);
-		wait1Msec(1000);
+		//setEnabled(wristPID,true);
+		while (SensorValue[Shoulder]!=a1*10 && SensorValue[Elbow]!=a2*10)
+		{
+				setShoulderValue(calculate(shoulderPID, SensorValue[Shoulder]));
+				setElbowValue(calculate(elbowPID, SensorValue[Elbow]));
+				//setWristValue(calculate(turnPID, SensorValue[])); look up how to use servos
+		}
 		setEnabled(shoulderPID,false);
 		setEnabled(elbowPID,false);
-		setEnabled(wristPID,false);
+		//setEnabled(wristPID,false);
 
-		//keepHandLevel(180-(a1+a2))
 
 }
 void turnBase()
@@ -79,17 +155,34 @@ void turnBase()
 
 task autonomous()
 {
-	int x = 0;
-	int y = bowlDist;
-	int count = 0;
-	while (true)
-	{
-			bendArm(x,y);
-			x = bowlDiam/2*sin(count);
-			y = (-1*(bowlDiam/2))*cos(count);
-			count++;
-	}
+		shoulderZero = SensorValue[Shoulder];
+		elbowZero 	 = SensorValue[Elbow];
+		wristZero		 = SensorValue[Wrist];
+		int x = 0;
+		int y = bowlDist;
+		int count = 0;
+		while (true)
+		{
+				if(SensorValue(Bumper)==1)
+				{
+					break;
+				}
+				bendArm(x,y);
+				x = bowlDiam/2*sin(count);
+				y = (-1*(bowlDiam/2))*cos(count);
+				count++;
+		}
 }
+
 task usercontrol()
 {
+		while (true)
+		{
+				setBaseValue(vexRT[Ch2]/12.7);
+				setShoulderValue(vexRT[Ch1]/-12.7);
+				setElbowValue(vexRT[Ch1]/12.7);
+				setWristVertValue(vexRT[Ch3]/12.7);
+				setWristRotValue(vexRT[Ch4]/12.7);
+				setClawValue(30*((vexRT[Btn5U]|vexRT[Btn5D])-(vexRT[Btn6U]|vexRT[Btn6D])));
+		}
 }
