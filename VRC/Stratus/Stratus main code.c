efd90be6e80682b309b82ebf8261465ec89801fd
erik <erik.ostlind@att.net>
2013-12-10 16:28:20 -0600

    added useful methods like drive arcade and SetArmSpeed. i also simplified your usercontroll method along with redesining your autonomus

--- VRC/Stratus/Stratus main code.c
+++ VRC/Stratus/Stratus main code.c
@@ -29,7 +29,19 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////
 
-
+void driveArcade(int y, int x)
+{
+	motor[frontLeft] = motor[backLeft] = y + x;
+	motor[frontRight] = motor[backRight] = y - x;
+}
+void setArmSpeed(int z)
+{
+	motor[armRight] = motor[armLeft] = motor[armLeft2] = motor[armRight2] = z;
+}
+void setIntakeSpeed(int speed)
+{
+		motor[intakeLeft] = motor[intakeRight] = speed;
+}
 void pre_auton()
 {
   // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
@@ -54,288 +66,191 @@ task autonomous()
   //if (SensorValue(touchSensor) == 0)
   //{
 	//Hits first friendly large ball into goal zone. (Total time: 1 second)
-bool outside=true;//set to false if behind bump,set to true if doing autonomous infront
-	if(outside == true)
-		{
-		ClearTimer(T1);
-	while(time1[T1] < 1000)
-	{
-  	motor[leftFront] = 80;
-  	motor[leftRear] = 80;
-  	motor[rightFront] = 80;
-  	motor[rightRear] = 80;
-  	motor[leftArm] = 70;
-  	motor[rightArm] = 70;
-  	motor[leftArm] = 70;
-  	motor[rightArm] = 70;
+//bool outside=true;//set to false if behind bump,set to true if doing autonomous infront
+//	if(outside == true)
+//		{
+//		ClearTimer(T1);
+//	while(time1[T1] < 1000)
+//	{
+//  	motor[leftFront] = 80;
+//  	motor[leftRear] = 80;
+//  	motor[rightFront] = 80;
+//  	motor[rightRear] = 80;
+//  	motor[leftArm] = 70;
+//  	motor[rightArm] = 70;
+//  	motor[leftArm] = 70;
+//  	motor[rightArm] = 70;
 
-	}
-		motor[leftFront] = 0;
-  	motor[leftRear] = 0;
-  	motor[rightFront] = 0;
-  	motor[rightRear] = 0;
-		//If a ball is in the robot, outtake
-	ClearTimer(T1);
-	while(time1[T1] < 2000)
-	{
-		motor[leftIntake] = 127;
-		motor[rightIntake] = 127;
+//	}
+//		motor[leftFront] = 0;
+//  	motor[leftRear] = 0;
+//  	motor[rightFront] = 0;
+//  	motor[rightRear] = 0;
+//		//If a ball is in the robot, outtake
+//	ClearTimer(T1);
+//	while(time1[T1] < 2000)
+//	{
+//		motor[leftIntake] = 127;
+//		motor[rightIntake] = 127;
 
-	}
+//	}
 
-	ClearTimer(T1);
-	while(time1[T1] < 2000)
-	{
-		motor[leftIntake] = -127;
-		motor[rightIntake] = -127;
-	}
+//	ClearTimer(T1);
+//	while(time1[T1] < 2000)
+//	{
+//		motor[leftIntake] = -127;
+//		motor[rightIntake] = -127;
+//	}
 
-	//5 point code. Confirmed to work for 5+, 10+ potential.
-	//Go back with arm up.
-	ClearTimer(T1);
-	while(time1[T1] < 900)
-	{
-  	motor[leftFront] = -100;
-  	motor[leftRear] = -100;
-  	motor[rightFront] = -100;
-  	motor[rightRear] = -100;
-  	motor[leftArm] = 10;
-  	motor[rightArm] = 10;
-  	motor[leftArm] = 10;
-  	motor[rightArm] = 10;
-	}
-	//Wait for five seconds and point robot in the direction of next ball.
-	ClearTimer(T1);
-	while(time1[T1] < 3000)
-	{
-  	motor[leftFront] = 0;
-  	motor[leftRear] = 0;
-		motor[rightFront] = 0;
-		motor[rightRear] = 0;
-		motor[leftArm] = 10;
-		motor[rightArm] = 10;
-		motor[leftArm] = 10;
-		motor[rightArm] = 10;
-		motor[leftIntake] = 0;
-		motor[rightIntake] = 0;
-	}
-	ClearTimer(T1);
-	while(time1[T1] < 200)
-	{
-		motor[leftArm] = -70;
-  	motor[rightArm] = -70;
-  	motor[leftArm] = -70;
-  	motor[rightArm] = -70;
-	}
+//	//5 point code. Confirmed to work for 5+, 10+ potential.
+//	//Go back with arm up.
+//	ClearTimer(T1);
+//	while(time1[T1] < 900)
+//	{
+//  	motor[leftFront] = -100;
+//  	motor[leftRear] = -100;
+//  	motor[rightFront] = -100;
+//  	motor[rightRear] = -100;
+//  	motor[leftArm] = 10;
+//  	motor[rightArm] = 10;
+//  	motor[leftArm] = 10;
+//  	motor[rightArm] = 10;
+//	}
+//	//Wait for five seconds and point robot in the direction of next ball.
+//	ClearTimer(T1);
+//	while(time1[T1] < 3000)
+//	{
+//  	motor[leftFront] = 0;
+//  	motor[leftRear] = 0;
+//		motor[rightFront] = 0;
+//		motor[rightRear] = 0;
+//		motor[leftArm] = 10;
+//		motor[rightArm] = 10;
+//		motor[leftArm] = 10;
+//		motor[rightArm] = 10;
+//		motor[leftIntake] = 0;
+//		motor[rightIntake] = 0;
+//	}
+//	ClearTimer(T1);
+//	while(time1[T1] < 200)
+//	{
+//		motor[leftArm] = -70;
+//  	motor[rightArm] = -70;
+//  	motor[leftArm] = -70;
+//  	motor[rightArm] = -70;
+//	}
 
-	//Go forward for two seconds.
-		ClearTimer(T1);
-	while(time1[T1] < 2200)
-	{
-		motor[leftArm] = 2;
-  	motor[rightArm] = 2;
-  	motor[leftArm] = 2;
-  	motor[rightArm] = 2;
-  motor[leftFront] = 80;
-  motor[leftRear] = 80;
-    motor[rightFront] = 80;
-    motor[rightRear] = 80;
-    motor[leftArm] = 30;
-  motor[rightArm] = 30;
-  motor[leftArm] = 30;
-  motor[rightArm] = 30;
-	}
+//	//Go forward for two seconds.
+//		ClearTimer(T1);
+//	while(time1[T1] < 2200)
+//	{
+//		motor[leftArm] = 2;
+//  	motor[rightArm] = 2;
+//  	motor[leftArm] = 2;
+//  	motor[rightArm] = 2;
+//  motor[leftFront] = 80;
+//  motor[leftRear] = 80;
+//    motor[rightFront] = 80;
+//    motor[rightRear] = 80;
+//    motor[leftArm] = 30;
+//  motor[rightArm] = 30;
+//  motor[leftArm] = 30;
+//  motor[rightArm] = 30;
+//	}
 
-	motor[leftFront] = 0;
-  motor[leftRear] = 0;
-    motor[rightFront] = 0;
-  motor[rightRear] = 0;
-    motor[leftArm] = 0;
-  motor[rightArm] = 0;
-  motor[leftArm] = 0;
-  motor[rightArm] = 0;
-  motor[leftIntake] = 0;
-		motor[rightIntake] = 0;
-
-}
-
+//	motor[leftFront] = 0;
+//  motor[leftRear] = 0;
+//    motor[rightFront] = 0;
+//  motor[rightRear] = 0;
+//    motor[leftArm] = 0;
+//  motor[rightArm] = 0;
+//  motor[leftArm] = 0;
+//  motor[rightArm] = 0;
+//  motor[leftIntake] = 0;
+//		motor[rightIntake] = 0;
+//}
 
 
 
-else //if behind bump (inside)
-{
-	//raise arm
-		motor[leftArm] = 65;
-		motor[rightArm] = 65;
-  	motor[leftArm] = 65;
-  	motor[rightArm] = 65;
-	wait1Msec(500);
-	//intake
-		motor[leftIntake] = 127;
-		motor[rightIntake] = 127;
+
+//else //if behind bump (inside)
+//{
+//	//raise arm
+//		motor[leftArm] = 65;
+//		motor[rightArm] = 65;
+//  	motor[leftArm] = 65;
+//  	motor[rightArm] = 65;
+//	wait1Msec(500);
+//	//intake
+//		motor[leftIntake] = 127;
+//		motor[rightIntake] = 127;
+//	wait1Msec(1000);
+//	//outtake
+//		motor[leftIntake] = -127;
+//		motor[rightIntake] = -127;
+//	wait1Msec(2000);
+//		motor[leftIntake] = 0;
+//		motor[rightIntake] = 0;
+//	//move forward and hit 3 buckyballs
+//		wait1Msec(2000);
+//		motor[leftArm] = -22;
+//		motor[rightArm] = -22;
+//  	motor[leftArm] = -22;
+//  	motor[rightArm] = -22;
+//  wait1Msec(800);
+//  	motor[leftArm] = 9;
+//		motor[rightArm] = 9;
+//  	motor[leftArm] = 9;
+//  	motor[rightArm] = 9;
+//  	motor[leftFront] = 80;
+// 	  motor[leftRear] = 80;
+//    motor[rightFront] = 80;
+// 	  motor[rightRear] = 80;
+//	wait1Msec(1500);
+//	  motor[leftArm] = 0;
+//		motor[rightArm] = 0;
+//  	motor[leftArm] = 0;
+//  	motor[rightArm] = 0;
+//		motor[leftFront] = 0;
+// 	  motor[leftRear] = 0;
+//    motor[rightFront] = 0;
+// 	  motor[rightRear] = 0;
+// 	  motor[leftIntake] = 0;
+//		motor[rightIntake] = 0;
+//}
+setIntakeSpeed(127);				//drop buckie ball
+
+	setArmSpeed(100);
 	wait1Msec(1000);
-	//outtake
-		motor[leftIntake] = -127;
-		motor[rightIntake] = -127;
-	wait1Msec(2000);
-		motor[leftIntake] = 0;
-		motor[rightIntake] = 0;
-	//move forward and hit 3 buckyballs
-		wait1Msec(2000);
-		motor[leftArm] = -22;
-		motor[rightArm] = -22;
-  	motor[leftArm] = -22;
-  	motor[rightArm] = -22;
-  wait1Msec(800);
-  	motor[leftArm] = 9;
-		motor[rightArm] = 9;
-  	motor[leftArm] = 9;
-  	motor[rightArm] = 9;
-  	motor[leftFront] = 80;
- 	  motor[leftRear] = 80;
-    motor[rightFront] = 80;
- 	  motor[rightRear] = 80;
-	wait1Msec(1500);
-	  motor[leftArm] = 0;
-		motor[rightArm] = 0;
-  	motor[leftArm] = 0;
-  	motor[rightArm] = 0;
-		motor[leftFront] = 0;
- 	  motor[leftRear] = 0;
-    motor[rightFront] = 0;
- 	  motor[rightRear] = 0;
- 	  motor[leftIntake] = 0;
-		motor[rightIntake] = 0;
+	setArmSpeed(5);
 
+	driveArcade(70,0);					//drive forward
+	wait1Msec(1100);
+	driveArcade(0,0);
+
+	setIntakeSpeed(0);
+	setArmSpeed(20);
+
+	wait1Msec(1000);
+
+	driveArcade(-70,0);				//drive back
+	wait1Msec(1200);
+	driveArcade(0,0);
+
+	wait1Msec(5000);
+
+	driveArcade(70,0);					//drive forward
+	wait1Msec(1300);
+	driveArcade(0,0);
+
+	wait1Msec(1000);
+
+	driveArcade(-70,0);				//drive back
+	wait1Msec(1300);
+	driveArcade(0,0);
 
 }
-/*  OLD CODE DO NOT USE UNDER ANY CIRCUMSTANCES
 
-
-	//15 point + enemy ball middle zone. Currently does not work, needs testing.
-	//Keeps arm lifted, turns to the right 90 degrees. (Total time: 1.5 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 500)
-	{
-	motor[leftFront] = 127;
-  motor[leftRear] = 127;
-  motor[rightFront] = -127;
-  motor[rightRear] = -127;
-	}
-	//Hits second friendly large ball into goal zone. (Total time: 2.5 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1000)
-	{
-  motor[leftFront] = 127;
-  motor[leftRear] = 127;
-  motor[rightFront] = 127;
-  motor[rightRear] = 127;
-	}
-	//Lowers arm in prep for going under barrier, turns to the left 90 degrees. (Total time: 3 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 500)
-	{
-  motor[leftFront] = -127;
-  motor[leftRear] = -127;
-  motor[rightFront] = 127;
-  motor[rightRear] = 127;
-  motor[leftArm] = -60;
-  motor[rightArm] = -60;
-  motor[leftArm] = -60;
-  motor[rightArm] = -60;
-	}
-	//Moves up to the goal. (Total time: 4 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1000)
-	{
-  motor[leftFront] = 127;
-  motor[leftRear] = 127;
-  motor[rightFront] = 127;
-  motor[rightRear] = 127;
-	}
-	//Lifts arm. (Total time: 5 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1000)
-	{
-		motor[leftArm] = 60;
-		motor[rightArm] = 60;
-		motor[leftArm]= 60;
-		motor[rightArm] = 60;
-	}
-	//Deposits preloads in goal. (Total time: 6.5 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1500)
-	{
-		motor[leftIntake] = 127;
-		motor[rightIntake] = 127;
-	}
-	//Turns to the right 120 degrees and lowers arm slightly. (Total time: 7 1/6 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 667)
-	{
-		motor[leftFront] = 127;
-		motor[leftRear] = 127;
-		motor[rightFront] = -127;
-		motor[rightRear] = -127;
-				motor[leftArm] = -20;
-		motor[rightArm] = -20;
-		motor[leftArm]= -20;
-		motor[rightArm] = -20;
-	}
-	//Hits enemy large ball into middle zone. (Total time: 8 2/3 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1500)
-	{
-		motor[leftFront] = 127;
-		motor[leftRear] = 127;
-		motor[rightFront] = 127;
-		motor[rightRear] = 127;
-	}
-	//Turns to the left 60 degrees. (Total time: 9 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 333)
-	{
-		motor[leftFront] = -127;
-		motor[leftRear] = -127;
-		motor[rightFront] = 127;
-		motor[rightRear] = 127;
-	}
-	//Hits other enemy large ball into middle zone. (Total time: 10 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 1000)
-	{
-		motor[leftFront] = 127;
-		motor[leftRear] = 127;
-		motor[rightFront] = 127;
-		motor[rightRear] = 127;
-	}
-	//Turns to the right 135 degrees and lowers arm. (Total time: 10.75 seconds)
-	ClearTimer(T1);
-	while(time1[T1] < 750)
-	{
-		motor[leftArm] = -60;
-		motor[rightArm] = -60;
-		motor[leftArm] = -60;
-		motor[rightArm] = -60;
-		motor[leftFront] = 127;
-		motor[leftRear] = 127;
-		motor[rightFront] = -127;
-		motor[rightRear] = -127;
-	}
-	//Travels as far as possible southwest while attempting to pick up more buckyballs. (Time out)
-	ClearTimer(T1);
-	while(time1[T1] < 4125)
-	{
-		motor[leftIntake] = 127;
-		motor[rightIntake] = 127;
-		motor[leftFront] = 127;
-		motor[leftRear] = 127;
-		motor[rightFront] = 127;
-		motor[rightRear] = 127;
-	}
-*/
-
-}
 /////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                 User Control Task
@@ -349,44 +264,44 @@ task usercontrol()
 	{        while (true)
         {
 
-          // This is the main execution loop for the user control program. Each time through the loop
-          // your program should update motor + servo values based on feedback from the joysticks.
+          			int driveX = vexRT[Ch4];
+							  int driveY = vexRT[Ch3];
+							  int armSpeed = vexRT[Ch2];
+							  int intakeSpeed = 127*((vexRT[Btn5U]|vexRT[Btn5D])-(vexRT[Btn6U]|vexRT[Btn6D]));
 
-          // .....................................................................................
-          // Insert user code here. This is where you use the joystick values to update your motors, etc.
-          // .....................................................................................
+							  if (abs(driveY) < 5) driveY = 0; // Deadband
+								if (abs(driveX) < 5) driveX = 0;
 
-                //Arm
-          //Use Left Stick
-
-
-
-
-                if (vexRT[Btn6U] == 1)
-                {
-                        motor[leftIntake] = 127;
-                        motor[rightIntake] = 127;
-                }
-                else if (vexRT[Btn6D] == 1)
-                {
-                        motor[leftIntake] = -127;
-                        motor[rightIntake] = -127;
-                }
-                else
-                {
-                        motor[leftIntake] = 0;
-                        motor[rightIntake] = 0;
-                }
-                //arm up and down Channel 3
-                motor[leftArm] = vexRT[Ch3];
-                motor[rightArm] = vexRT[Ch3];
-                //Turning/Straight driving
-                //Use Right-Stick
+								driveArcade(driveY, driveX);
+							  setArmSpeed(armSpeed);
+							  setIntakeSpeed(intakeSpeed);
 
 
-                        motor[rightFront] = (vexRT[Ch2] + vexRT[Ch1]);
-                        motor[rightRear] = (vexRT[Ch2] + vexRT[Ch1]);
-                        motor[leftRear] = (vexRT[Ch2] - vexRT[Ch1]);
-                        motor[leftFront]= (vexRT[Ch2] - vexRT[Ch1]);
+                //if (vexRT[Btn6U] == 1)
+                //{
+                //        motor[leftIntake] = 127;
+                //        motor[rightIntake] = 127;
+                //}
+                //else if (vexRT[Btn6D] == 1)
+                //{
+                //        motor[leftIntake] = -127;
+                //        motor[rightIntake] = -127;
+                //}
+                //else
+                //{
+                //        motor[leftIntake] = 0;
+                //        motor[rightIntake] = 0;
+                //}
+                ////arm up and down Channel 3
+                //motor[leftArm] = vexRT[Ch3];
+                //motor[rightArm] = vexRT[Ch3];
+                ////Turning/Straight driving
+                ////Use Right-Stick
+
+
+                //        motor[rightFront] = (vexRT[Ch2] + vexRT[Ch1]);
+                //        motor[rightRear] = (vexRT[Ch2] + vexRT[Ch1]);
+                //        motor[leftRear] = (vexRT[Ch2] - vexRT[Ch1]);
+                //        motor[leftFront]= (vexRT[Ch2] - vexRT[Ch1]);
                }
 }
